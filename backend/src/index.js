03e81485117de941bf5ebd30c2965d6fdcd41ddd
Ananta Debnath <ananta.deb2005@gmail.com>
2026-07-11 12:29:38 +0600

    Backend endpoint for agent

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,14 @@ try {
 
 const express = require('express');
 const healthRoutes = require('./routes/healthRoutes.js');
+const agentRoute = require('./routes/agentRoute.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/', healthRoutes);
+app.use('/', agentRoute);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on http://localhost:${PORT}`);
